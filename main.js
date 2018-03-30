d7ddad64873420fac027e3e4dc010499047fec2d
olengai97 <36862078+olengai97@users.noreply.github.com>
2018-03-30 12:22:41 +0300

    reupdated

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var div = document.querySelector('div');
 
 var req = new XMLHttpRequest();
 req.open('GET', 'https://olengai97.github.io/oleeprojects/pets.json');
-div.innerHTML = req.responseText;
+req.onload = function() {
+	var dat = req.responseText;
+	div.innerHTML = dat;
+}
 	
 req.send();
 
