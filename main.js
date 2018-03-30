3ebef963db36824d01e251ee356e31dfc9281a90
olengai97 <36862078+olengai97@users.noreply.github.com>
2018-03-30 12:20:00 +0300

    localhost removed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var div = document.querySelector('div');
 
 var req = new XMLHttpRequest();
-req.open('GET', 'https://localhost/ajax/pets.json');
+req.open('GET', 'https://olengai97.github.io/oleeprojects/pets.json');
 div.innerHTML = req.responseText;
 	
 req.send();
